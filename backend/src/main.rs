cddae97e9d26174682015d7de2e38be81ba76aeb
Falco90 <falcorodenburg@gmail.com>
2025-07-09 14:28:54 +0200

    add insert query example

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,33 @@
-use std::error::Error;
-use std::env;
-use sqlx::Row;
 use dotenv::dotenv;
+use sqlx::migrate::Migrator;
+use sqlx::{Row, postgres::PgPoolOptions};
+use std::env;
+use std::error::Error;
+use std::path::Path;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
 
-    let url = env::var("DATABASE_URL").to_owned()?;
-    let pool = sqlx::postgres::PgPool::connect(&url).await?;
+    let url = env::var("DATABASE_URL")?;
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&url)
+        .await?;
 
-    sqlx::migrate!("./migrations").run(&pool).await?;
+    // Runtime migrations (no macro!)
+    let migrator = Migrator::new(Path::new("./migrations")).await?;
+    migrator.run(&pool).await?;
     println!("Migrations applied successfully.");
-    
+
+    // Insert course
+    sqlx::query("INSERT INTO course (title, creator, num_students) VALUES ($1, $2, $3)")
+        .bind("Thai Language for Beginners")
+        .bind("Boss")
+        .bind(0)
+        .execute(&pool)
+        .await?;
+    println!("Course added successfully!");
+
     Ok(())
 }
