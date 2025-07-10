2c2ecf5c9edc12ed8190d1837c08bd2a7fcfbb49
Falco90 <falcorodenburg@gmail.com>
2025-07-10 09:58:54 +0200

    add axum routing, create endpoint for course creation, separate migrations function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,33 +1,60 @@
+use axum::{Json, Router, extract::State, routing::post, http::StatusCode, response::IntoResponse};
 use dotenv::dotenv;
-use sqlx::migrate::Migrator;
-use sqlx::{Row, postgres::PgPoolOptions};
-use std::env;
-use std::error::Error;
-use std::path::Path;
+use serde::Deserialize;
+use serde_json::json;
+use sqlx::{Pool, Postgres, postgres::PgPoolOptions, migrate::Migrator};
+use std::{env, error::Error, net::SocketAddr, path::Path};
+use tokio::net::TcpListener;
+
+#[derive(Deserialize)]
+struct NewCourse {
+    title: String,
+    creator: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
 
     let url = env::var("DATABASE_URL")?;
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&url)
-        .await?;
+    let pool = PgPoolOptions::new().connect(&url).await?;
 
-    // Runtime migrations (no macro!)
-    let migrator = Migrator::new(Path::new("./migrations")).await?;
-    migrator.run(&pool).await?;
-    println!("Migrations applied successfully.");
+    let app = Router::new()
+        .route("/create-course", post(create_course))
+        .with_state(pool);
 
-    // Insert course
-    sqlx::query("INSERT INTO course (title, creator, num_students) VALUES ($1, $2, $3)")
-        .bind("Thai Language for Beginners")
-        .bind("Boss")
-        .bind(0)
-        .execute(&pool)
-        .await?;
-    println!("Course added successfully!");
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 
     Ok(())
+}
+
+async fn run_migrations(pool: &Pool<Postgres>) -> Result<(), Box<dyn Error>> {
+    let migrator = Migrator::new(Path::new("./migrations")).await?;
+    migrator.run(pool).await?;
+
+    println!("Migrations applied successfully.");
+
+    Ok(())
+}
+
+async fn create_course(
+    State(pool): State<Pool<Postgres>>,
+    Json(payload): Json<NewCourse>,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let _ = sqlx::query("INSERT INTO course (title, creator, num_students) VALUES ($1, $2, $3)")
+        .bind(&payload.title)
+        .bind(&payload.creator)
+        .bind(0)
+        .execute(&pool)
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    Ok((
+        StatusCode::CREATED,
+        Json(json!({ "message": "Course created successfully" })),
+    ))
 }
