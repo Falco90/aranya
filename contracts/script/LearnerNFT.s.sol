7e8ae1fe5786088788709724615c8bb3bccd435a
Falco90 <falcorodenburg@gmail.com>
2025-07-22 15:01:48 +0700

    setup basic LearnerNFT contract and FDC scripts

---
+++ contracts/script/LearnerNFT.s.sol
@@ -0,0 +1,256 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {console} from "dependencies/forge-std-1.9.5/src/console.sol";
+import {Script} from "dependencies/forge-std-1.9.5/src/Script.sol";
+import {Strings} from "@openzeppelin-contracts/utils/Strings.sol";
+import {Base} from "./Base.s.sol";
+import {Base as StringsBase} from "src/utils/fdcStrings/Base.sol";
+import {IWeb2Json} from "dependencies/flare-periphery-0.1.33/src/coston2/IWeb2Json.sol";
+import {Surl} from "dependencies/surl-0.0.0/src/Surl.sol";
+import {ILearnerNFT, LearnerNFT} from "src/LearnerNFT.sol";
+
+string constant attestationTypeName = "LearnerNFT";
+string constant dirPath = "data/";
+
+contract PrepareAttestationRequest is Script {
+    using Surl for *;
+
+    // Setting request data
+    // string public apiUrl = "https://swapi.dev/api/people/3/";
+    string public apiUrl = "https://swapi.info/api/people/3";
+    string public httpMethod = "GET";
+    // Defaults to "Content-Type": "application/json"
+    string public headers = '{\\"Content-Type\\":\\"text/plain\\"}';
+    string public queryParams = "{}";
+    string public body = "{}";
+    string public postProcessJq =
+        '{name: .name, height: .height, mass: .mass, numberOfFilms: .films | length, uid: (.url | split(\\"/\\") | .[-1] | tonumber)}';
+    string public abiSignature =
+        '{\\"components\\": [{\\"internalType\\": \\"string\\", \\"name\\": \\"name\\", \\"type\\": \\"string\\"},{\\"internalType\\": \\"uint256\\", \\"name\\": \\"height\\", \\"type\\": \\"uint256\\"},{\\"internalType\\": \\"uint256\\", \\"name\\": \\"mass\\", \\"type\\": \\"uint256\\"},{\\"internalType\\": \\"uint256\\", \\"name\\": \\"numberOfFilms\\", \\"type\\": \\"uint256\\"},{\\"internalType\\": \\"uint256\\", \\"name\\": \\"uid\\", \\"type\\": \\"uint256\\"}],\\"name\\": \\"task\\",\\"type\\": \\"tuple\\"}';
+
+    string public sourceName = "PublicWeb2";
+
+    function prepareRequestBody(
+        string memory url,
+        string memory httpMethod,
+        string memory headers,
+        string memory queryParams,
+        string memory body,
+        string memory postProcessJq,
+        string memory abiSignature
+    ) private pure returns (string memory) {
+        return
+            string.concat(
+                '{"url": "',
+                url,
+                '","httpMethod": "',
+                httpMethod,
+                '","headers": "',
+                headers,
+                '","queryParams": "',
+                queryParams,
+                '","body": "',
+                body,
+                '","postProcessJq": "',
+                postProcessJq,
+                '","abiSignature": "',
+                abiSignature,
+                '"}'
+            );
+    }
+
+    function run() external {
+        // Preparing request data
+        string memory attestationType = Base.toUtf8HexString(
+            attestationTypeName
+        );
+        string memory sourceId = Base.toUtf8HexString(sourceName);
+        string memory requestBody = prepareRequestBody(
+            apiUrl,
+            httpMethod,
+            headers,
+            queryParams,
+            body,
+            postProcessJq,
+            abiSignature
+        );
+        (string[] memory headers, string memory body) = Base
+            .prepareAttestationRequest(attestationType, sourceId, requestBody);
+
+        // string memory baseUrl = "https://testnet-verifier-fdc-test.aflabs.org/";
+        string memory baseUrl = vm.envString("WEB2JSON_VERIFIER_URL_TESTNET");
+        string memory url = string.concat(
+            baseUrl,
+            "Web2Json",
+            "/prepareRequest"
+        );
+        console.log("url: %s", url);
+
+        // Posting the attestation request
+        (, bytes memory data) = url.post(headers, body);
+
+        Base.AttestationResponse memory response = Base.parseAttestationRequest(
+            data
+        );
+
+        // Writing abiEncodedRequest to a file
+        Base.writeToFile(
+            dirPath,
+            string.concat(attestationTypeName, "_abiEncodedRequest"),
+            StringsBase.toHexString(response.abiEncodedRequest),
+            true
+        );
+    }
+}
+
+contract SubmitAttestationRequest is Script {
+    using Surl for *;
+
+    function run() external {
+        // Reading the abiEncodedRequest from a file
+        string memory fileName = string.concat(
+            attestationTypeName,
+            "_abiEncodedRequest",
+            ".txt"
+        );
+        string memory filePath = string.concat(dirPath, fileName);
+        string memory requestStr = vm.readLine(filePath);
+        bytes memory request = vm.parseBytes(requestStr);
+
+        // Submitting the attestation request
+        uint256 timestamp = Base.submitAttestationRequest(request);
+        uint256 votingRoundId = Base.calculateRoundId(timestamp);
+
+        // Writing to a file
+        Base.writeToFile(
+            dirPath,
+            string.concat(attestationTypeName, "_votingRoundId"),
+            Strings.toString(votingRoundId),
+            true
+        );
+    }
+}
+
+contract RetrieveDataAndProof is Script {
+    using Surl for *;
+
+    function run() external {
+        string memory daLayerUrl = vm.envString("COSTON2_DA_LAYER_URL"); // XXX
+        string memory apiKey = vm.envString("X_API_KEY");
+
+        // We import the abiEncodedRequest and votingRoundId from the files
+        string memory requestBytes = vm.readLine(
+            string.concat(
+                dirPath,
+                attestationTypeName,
+                "_abiEncodedRequest",
+                ".txt"
+            )
+        );
+        string memory votingRoundId = vm.readLine(
+            string.concat(
+                dirPath,
+                attestationTypeName,
+                "_votingRoundId",
+                ".txt"
+            )
+        );
+
+        console.log("votingRoundId: %s\n", votingRoundId);
+        console.log("requestBytes: %s\n", requestBytes);
+
+        // Preparing the proof request
+        string[] memory headers = Base.prepareHeaders(apiKey);
+        string memory body = string.concat(
+            '{"votingRoundId":',
+            votingRoundId,
+            ',"requestBytes":"',
+            requestBytes,
+            '"}'
+        );
+        console.log("body: %s\n", body);
+        console.log(
+            "headers: %s",
+            string.concat("{", headers[0], ", ", headers[1]),
+            "}\n"
+        );
+
+        // Posting the proof request
+        string memory url = string.concat(
+            daLayerUrl,
+            // "api/v0/fdc/get-proof-round-id-bytes"
+            "api/v1/fdc/proof-by-request-round-raw"
+        );
+        console.log("url: %s\n", url);
+
+        (, bytes memory data) = Base.postAttestationRequest(url, headers, body);
+
+        // Decoding the response from JSON data
+        bytes memory dataJson = Base.parseData(data);
+        Base.ParsableProof memory proof = abi.decode(
+            dataJson,
+            (Base.ParsableProof)
+        );
+
+        IWeb2Json.Response memory proofResponse = abi.decode(
+            proof.responseHex,
+            (IWeb2Json.Response)
+        );
+
+        IWeb2Json.Proof memory _proof = IWeb2Json.Proof(
+            proof.proofs,
+            proofResponse
+        );
+
+        // Writing proof to a file
+        Base.writeToFile(
+            dirPath,
+            string.concat(attestationTypeName, "_proof"),
+            StringsBase.toHexString(abi.encode(_proof)),
+            true
+        );
+    }
+}
+
+contract DeployContract is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        LearnerNFT learnerNFT = new LearnerNFT("TestCourse", "Test", 4, msg.sender);
+        address _address = address(learnerNFT);
+
+        vm.stopBroadcast();
+
+        Base.writeToFile(
+            dirPath,
+            string.concat(attestationTypeName, "_address"),
+            StringsBase.toHexString(abi.encodePacked(_address)),
+            true
+        );
+    }
+}
+
+contract InteractWithContract is Script {
+    function run() external {
+        string memory addressString = vm.readLine(
+            string.concat(dirPath, attestationTypeName, "_address", ".txt")
+        );
+        address _address = vm.parseAddress(addressString);
+        string memory proofString = vm.readLine(
+            string.concat(dirPath, attestationTypeName, "_proof", ".txt")
+        );
+        bytes memory proofBytes = vm.parseBytes(proofString);
+        IWeb2Json.Proof memory proof = abi.decode(
+            proofBytes,
+            (IWeb2Json.Proof)
+        );
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        ILearnerNFT learnerNFT = ILearnerNFT(_address);
+        uint256 tokenId = learnerNFT.mint();
+        learnerNFT.updateMilestone(tokenId, proof);
+        vm.stopBroadcast();
+    }
+}
---
+++ contracts/src/LearnerNFT.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ContractRegistry} from "flare-periphery/src/coston2/ContractRegistry.sol";
+import {IWeb2Json} from "flare-periphery/src/coston2/IWeb2Json.sol";
+
+interface ILearnerNFT {
+    function updateMilestone(
+        uint256 tokenId,
+        IWeb2Json.Proof calldata data
+    ) external;
+
+    function mint() external returns (uint256);
+}
+
+contract LearnerNFT is ERC721, ERC721URIStorage {
+    uint256 public tokenCounter;
+    uint256 public courseId;
+    address public courseCreator;
+
+    mapping(uint256 => uint256) public tokenMilestones;
+    mapping(uint256 => bool) public tokenExists;
+
+    event LearnerNFTMinted(address indexed learner, uint256 indexed tokenId);
+
+    event MilestoneUpdated(
+        address indexed learner,
+        uint256 indexed tokenId,
+        uint256 newLevel
+    );
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _courseId,
+        address _creator
+    ) ERC721(_name, _symbol) {
+        courseId = _courseId;
+        courseCreator = _creator;
+    }
+
+    function isJsonApiProofValid(
+        IWeb2Json.Proof calldata _proof
+    ) private view returns (bool) {
+        // Inline the check for now until we have an official contract deployed
+        return ContractRegistry.getFdcVerification().verifyJsonApi(_proof);
+    }
+
+    function mint() external returns (uint256) {
+        uint256 tokenId = ++tokenCounter;
+        _safeMint(msg.sender, tokenId);
+        tokenExists[tokenId] = true;
+        emit LearnerNFTMinted(msg.sender, tokenId);
+        return tokenId;
+    }
+
+    function updateMilestone(
+        uint256 tokenId,
+        IWeb2Json.Proof calldata proof
+    ) external {
+        require(ownerOf(tokenId) == msg.sender, "Not token owner");
+        require(isJsonApiProofValid(proof), "Invalid proof");
+
+        uint256 newMilestone = abi.decode(
+            proof.data.responseBody.abiEncodedData,
+            (uint256)
+        );
+        tokenMilestones[tokenId] = newMilestone;
+
+        emit MilestoneUpdated(msg.sender, tokenId, newMilestone);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
